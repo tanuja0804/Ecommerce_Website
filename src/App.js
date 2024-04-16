e0e7f827cea6c89451e5cfa33b8491f2ab1b9ce0
tanuja0804 <tanujamortha@gmail.com>
2024-04-16 19:10:02 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Shop from './Pages/Shop';
-import { ShopCategory } from './Pages/ShopCategory';
+import ShopCategory from './Pages/ShopCategory';
 import Product from './Pages/Product';
 import Cart from './Pages/Cart';
 import LoginSignup from './Pages/LoginSignup';
+import Footer from './Components/Footer/Footer';
+import men_banner from './Components/Assets/banner_mens.png'
+import women_banner from './Components/Assets/banner_women.png'
+import kid_banner from './Components/Assets/banner_kids.png'
+
+
 
 function App() {
     return (
@@ -14,15 +20,16 @@ function App() {
                 <Navbar />
                 <Routes>
                     <Route path='/shop' element={<Shop />} />
-                    <Route path='/bicycle' element={<ShopCategory category="bicycle" />} />
-                    <Route path='/mattresses' element={<ShopCategory category="mattresses" />} />
-                    <Route path='/electronics' element={<ShopCategory category="electronics" />} />
+                    <Route path='/bicycle' element={<ShopCategory banner={men_banner} category="bicycle" />} />
+                    <Route path='/mattresses' element={<ShopCategory banner={women_banner} category="mattresses" />} />
+                    <Route path='/electronics' element={<ShopCategory banner={kid_banner} category="electronics" />} />
                     <Route path='/product' element={<Product />}>
                         <Route path=':productId' element={<Product />} />
                     </Route>
                     <Route path='/cart' element={<Cart />} />
                     <Route path='/login' element={<LoginSignup />} />
                 </Routes>
+                <Footer />
             </BrowserRouter>
         </div>
     );
--- src/Components/NewsLetter/NewsLetter.jsx
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './NewsLetter.css'
+
 const NewsLetter = () => {
   return (
     <div className='news-letter'>
         <h1>Get Exclusive Offers</h1>
-        <p>Stay Updated</p>
+        <p>Subscribe to our NewsLetter and Stay Updated</p>
         <div>
-            
+        <input type="email" placeholder='Your Email Id' />
+            <button>Subscribe</button>
         </div>
     </div>
   )
